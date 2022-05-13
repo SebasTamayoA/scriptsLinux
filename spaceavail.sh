ecb0f10db3ab1d9540ebe1e8de5ddb32876f09c9
Sebastián Tamayo <88455504+SebasTamayoA@users.noreply.github.com>
2022-05-13 07:02:32 -0500

    Update spaceavail.sh

--- spaceavail.sh
+++ spaceavail.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Porcentaje de uso de disco duro
+# Percentage of hard disk usage
 
 used=$(df / | grep / | awk '{print $5}' | sed 's/%//g')
 
-# si esta en el rango 1 y 40% de uso
+# 1 to 40% usage
 if [ $used -ge 1 ] && [ $used -le 40 ]; then
-    echo "El disco duro esta ok, el porcentaje de uso es: $used%"
-# si esta en el rango 41 y 80% de uso
+    echo "The hard disk is ok, the percentage of use is: $used%"
+# 41 to 80% usage
 elif [ $used -ge 41 ] && [ $used -le 80 ]; then
-    echo "Advertencia!, el disco duro esta regular, el porcentaje de uso es: $used%"
-# si esta en el rango 81 y 100% de uso
+    echo "Warning!, the hard disk is regular, the percentage of use is: $used%"
+# 81 y 100% usage
 elif [ $used -ge 81 ] && [ $used -le 100 ]; then
-    echo "El disco duro esta lleno, el porcentaje de uso es: $used%"
+    echo "The hard disk is full, the usage percentage is: $used%"
 fi
